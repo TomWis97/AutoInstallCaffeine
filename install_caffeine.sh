508a9411b455082c2ed39c2426e0a6bf8f61dced
TomWis97 <github@tomwishaupt.nl>
2016-10-06 08:33:44 +0200

    Added comments.

--- install_caffeine.sh
+++ install_caffeine.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
+# Automatically install and permanently activate the Caffeine Gnome extension.
+# Author:  Tom Wishaupt
+# Date:    6-10-2016
+# Version: 0.1
+# Notes:   This script uses a hardcoded download URL. If the current version is not
+#          is not compatible the the shell version you're running, update the URL.
+#          Currently uses gsettings. Although it should still work, updating to dconf
+#          is adviseable.
+#
 #TODO Check if Caffeine is already installed.
+#TODO Use dconf instead of gsettings.
 # Source: http://bernaerts.dyndns.org/linux/76-gnome/345-gnome-shell-install-remove-extension-command-line-script
 
 # Set variables so we can easily change them if necessary.
-install_path="$HOME/.local/share/gnome-shell/extensions"
-#gnome_version="$(DISPLAY=":0" gnome-shell --version | tr -cd "0-9.")"
-extensions_site="https://extensions.gnome.org"
-# Setting variables 
-extension_id="517"
 extension_uuid="caffeine@patapon.info"
 extension_downloadurl="/download-extension/caffeine@patapon.info.shell-extension.zip?version_tag=5712"
+install_path="$HOME/.local/share/gnome-shell/extensions"
+extensions_site="https://extensions.gnome.org"
 # Done setting variables.
 echo "Gnome version $gnome_version found."
-# Download zip.
+# Download zip to /tmp.
 wget -O /tmp/caffeine-extension.zip "$extensions_site$extension_downloadurl"
 # Create directory.
 mkdir -p "$install_path/$extension_uuid"
@@ -25,9 +32,9 @@ rm /tmp/caffeine-extension.zip
 # list enabled extensions
 ext_list=$(gsettings get org.gnome.shell enabled-extensions | sed 's/^.\(.*\).$/\1/')
 
-# if extension not already enabled, declare it
+# If extension not already enabled, declare it
 ext_enabled=$(echo ${ext_list} | grep ${extension_uuid})
 [ "$ext_enabled" = "" ] && gsettings set org.gnome.shell enabled-extensions "[${ext_list},'${extension_uuid}']"
 
-#After we're done, we'll need to restart Gnome.
+#After we're done, restart Gnome.
 gnome-shell --replace &
